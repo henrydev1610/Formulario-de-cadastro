2435626302e58d8df4234e152e3b519c1888e7c6
Henry Benjamin <henrybenjamin.dev@gamil.com>
2024-01-29 15:05:21 -0300

    criando a validacao em js

---
+++ scripts/script.js
@@ -0,0 +1,86 @@
+const form = document.getElementById('form'),
+    username = document.getElementById('username'),
+    email = document.getElementById('email'),
+    password = document.getElementById('password'),
+    passowrdConfirm = document.getElementById('password-confirm') 
+
+
+
+form.addEventListener('submit',(e)=>{
+        e.preventDefault()
+    checkInputUsername();
+    checkInputEmail();
+    checkInputPassword();
+    checkPasswordConfirm();
+
+    alert('cadastrado com sucesso!')
+
+
+
+})
+
+function checkInputUsername(){
+  const usernameValue = username.value;
+  if(usernameValue == ""){
+    errorInput(username, "Preencha o Campo!");
+  }else{
+    const formItem = username.parentElement;
+    formItem.classList = 'form-content'
+  }
+}
+
+
+
+function checkInputEmail(){
+    const emailValue = email.value
+
+    if(emailValue == ""){
+        errorInput(email, 'prencha o campo de E-mail!');
+    }else{
+        formItem = email.parentElement;
+        formItem.classList = 'form-content'
+    }
+
+}
+
+function checkInputPassword(){
+    const passwordValue = password.value;
+    if(passwordValue == ''){
+        errorInput(password, 'A senha é obrigatória!')
+    }else if(passwordValue.length <8){
+        errorInput(password, 'Senha precisa ter no mínimo 8 caracteres!')
+    }else{
+        const formItem = password.parentElement;
+        formItem.classList = 'form-content'
+    }
+}
+
+
+function checkPasswordConfirm(){
+    const passwordValue = password.value
+    const passowrdConfirmValue = passowrdConfirm.value 
+    if(passowrdConfirmValue == ''){
+        errorInput(passowrdConfirm, 'Insira a senha de confirmação')
+    }
+    else if(passowrdConfirmValue != passwordValue){
+        errorInput(passowrdConfirm, 'As senhas não correspondem')
+    }else{
+        const formItem = passowrdConfirm.parentElement;
+        formItem.classList = 'form-content'
+    }
+}
+
+
+
+
+
+
+
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector('a');
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error";
+
+}
